4ffedc372166282af1ceba5c6a6cdb75ff8cdbf9
tabeer123 <tabeersohail786@gmail.com>
2018-04-20 23:38:18 +0500

    WindowsFormsApplication2.Form1.resources

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lnkReset = new System.Windows.Forms.LinkLabel();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.cmdRegistration = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lnklblLogin = new System.Windows.Forms.LinkLabel();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lnkReset
+            // 
+            this.lnkReset.AutoSize = true;
+            this.lnkReset.Location = new System.Drawing.Point(119, 195);
+            this.lnkReset.Name = "lnkReset";
+            this.lnkReset.Size = new System.Drawing.Size(35, 13);
+            this.lnkReset.TabIndex = 19;
+            this.lnkReset.TabStop = true;
+            this.lnkReset.Text = "Reset";
+            this.lnkReset.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkReset_LinkClicked);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(157, 105);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 18;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(157, 55);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 17;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // cmdRegistration
+            // 
+            this.cmdRegistration.Location = new System.Drawing.Point(182, 167);
+            this.cmdRegistration.Name = "cmdRegistration";
+            this.cmdRegistration.Size = new System.Drawing.Size(75, 23);
+            this.cmdRegistration.TabIndex = 16;
+            this.cmdRegistration.Text = "Login";
+            this.cmdRegistration.UseVisualStyleBackColor = true;
+            this.cmdRegistration.Click += new System.EventHandler(this.cmdRegistration_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(27, 105);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 15;
+            this.lblPassword.Text = "Password";
+            this.lblPassword.Click += new System.EventHandler(this.lblPassword_Click);
+            // 
+            // lnklblLogin
+            // 
+            this.lnklblLogin.AutoSize = true;
+            this.lnklblLogin.Location = new System.Drawing.Point(27, 172);
+            this.lnklblLogin.Name = "lnklblLogin";
+            this.lnklblLogin.Size = new System.Drawing.Size(58, 13);
+            this.lnklblLogin.TabIndex = 14;
+            this.lnklblLogin.TabStop = true;
+            this.lnklblLogin.Text = "registration";
+            this.lnklblLogin.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnklblLogin_LinkClicked);
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(27, 63);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 13;
+            this.lblUsername.Text = "Username";
+            this.lblUsername.Click += new System.EventHandler(this.lblUsername_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lnkReset);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.cmdRegistration);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lnklblLogin);
+            this.Controls.Add(this.lblUsername);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel lnkReset;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button cmdRegistration;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.LinkLabel lnklblLogin;
+        private System.Windows.Forms.Label lblUsername;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdRegistration_Click(object sender, EventArgs e)
+        {
+            Server.Service1 server = new Server.Service1();
+            bool isvalid;
+            bool isvalidPassed;
+            Server.isvalid(txtUsername.Text, txtPassword.Text, out isvalid, out isvalidPassed);
+            if (isvalid)
+            {
+                MessageBox.Show("valid user!");
+            }
+            else
+            {
+                MessageBox.Show("invalid user!");
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lnkReset_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Reset reset = new Reset();
+            this.Hide();
+            reset.Show();
+           
+        }
+
+        private void lblPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lnklblLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form1 registration = new Form1();
+            this.Hide();
+            registration.Show();
+
+        }
+
+        private void lblUsername_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
